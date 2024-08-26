c92b974fd8eeb0812519a095c5996e61a6a9d102
Safarov12 <147270022+Safarov12@users.noreply.github.com>
2024-08-26 21:40:49 +0500

    js
    Поправила макет: хэдер корзина, все карточки съехавшие выстроила по размерам, стрелки поставила на место.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,16 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+
   title = 'mentoring-first-project';
+  readonly HeaderItem1 = 'Главная';
+  readonly HeaderItem2 = 'О компании';
+  readonly HeaderItem3 = 'Каталог';
+
+  readonly BarItem1 = 'Каталог';
+  readonly BarItem2 = 'Стройматериалы';
+  readonly BarItem3 = 'Инструменты';
+  readonly BarItem4 = 'Электроника';
+  readonly BarItem5 = 'Интерьер и одежда';
+  readonly BarLocation = 'Москва';
 }
