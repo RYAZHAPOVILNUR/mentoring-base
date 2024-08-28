046b696c1532f683f9f342d80828c70ade114ab3
Proxima <nanamofeevn@gmail.com>
2024-08-28 22:57:26 +0300

    task NgIf NgFor ~

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,51 @@
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+const newPages = [5, 4, 3, 2, 1];
+const funcl = (aboutCompany: string) => {
+  return (aboutCompany)
+}
+funcl('О компании');
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  
 })
+
 export class AppComponent {
   title = 'mentoring-first-project';
 
+  newPages = newPages;
+
+  isShowCatalog = true; 
+
+  isShowSadMan = true;
+
+  isUppercase = true;
+
   readonly headerItemFirst = 'Главная';
-  readonly headerItemSecond = 'О компании';
-  readonly headerItemThird = 'Каталог';
+  aboutCompany = 'O компании';
+  headerItemThird = 'Каталог';
+  headerTwoItemFirst = 'Каталог';
+  headerTwoItemSecond = 'Стройматериалы';
+  headerTwoItemThird = 'Инструменты';
+  headerTwoItemFourth = 'Электрика';
+  headerTwoItemFifth = 'Интерьер и одежда';
 
-  readonly headerTwoItemFirst = 'Каталог';
-  readonly headerTwoItemSecond = 'Стройматериалы';
-  readonly headerTwiItemThird = 'Инструменты';
-  readonly headerTwiItemFourth = 'Электрика';
-  readonly headerTwiItemFifth = 'Интерьер и одежда';
+
+  menuItems = ['Каталог', 'Стройматериалы',  'Инструменты', 'Электрика', 'Интерьер и одежда'];
+  upperCaseMenuItems = this.menuItems;
+  isUpperCase = true;
+  changeMenuText() {
+    this.menuItems = this.upperCaseMenuItems.map(
+      (item: any) => this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
+    )
+    this.isUpperCase = !this.isUpperCase
+  }
 }
-
