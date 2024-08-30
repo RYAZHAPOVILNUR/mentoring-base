ed17cc737b5379a4350de5264c100268dd7c7bd6
Safarov12 <147270022+Safarov12@users.noreply.github.com>
2024-08-30 15:51:05 +0500

    Дз Js
    Исправила в коде мелкие ошибки, выявленные в прошлом код ревью. Перенесла стили прописанные в html в css. Выполнила домашние задания 1-3, 4 тоже, но не понимаю почему оно не работает

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,66 @@
+import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf,NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
 export class AppComponent {
+
+  readonly  aboutCompany = (item: string) => {
+    return item 
+  }
+  readonly aboutCompanyResult = this.aboutCompany('О компании');
+
+  isShowCatalog = true;
+  isShowBanner = true;
+
+  readonly  newPages = [5,4,3,2,1];
+  readonly pagesNew = this.newPages.map(
+  (item) => {
+    return item;
+  }
+  )
+
+  MenuItems = ['Каталог', ' Стройматериалы',' Инструменты',' Электроника',' Интерьер и одежда']
+
+ readonly upperCaseMenuItems = this.MenuItems.map (
+   (item: string) => {
+     return item.toUpperCase();
+   }
+  )
+
+isUpperCase = true;
+
+  changeMenuText () {
+    const upperCaseMenuItems = this.MenuItems.map(
+      (item: string) => this.isUpperCase ? item.toLowerCase(): item.toUpperCase()
+      )
+      this.isUpperCase = !this.isUpperCase
+    }
 
   title = 'mentoring-first-project';
   readonly HeaderItem1 = 'Главная';
-  readonly HeaderItem2 = 'О компании';
+  readonly HeaderItem2 = this.aboutCompanyResult;
   readonly HeaderItem3 = 'Каталог';
 
-  readonly BarItem1 = 'Каталог';
-  readonly BarItem2 = 'Стройматериалы';
-  readonly BarItem3 = 'Инструменты';
-  readonly BarItem4 = 'Электроника';
-  readonly BarItem5 = 'Интерьер и одежда';
+  readonly BarItem1 = this.MenuItems [0];
+  readonly BarItem2 = this.MenuItems [1];
+  readonly BarItem3 = this.MenuItems [2];
+  readonly BarItem4 = this.MenuItems [3];
+  readonly BarItem5 = this.MenuItems [4];
   readonly BarLocation = 'Москва';
+
+}
+
+const name = 'Nika'
+if (name === 'Nika') {
+  console.log ('Name is Nika') 
+} else {
+  console.log ('ERROR')
 }
