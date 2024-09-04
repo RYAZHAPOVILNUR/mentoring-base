86f9217f17df043d8dcacc57bc98dd67b4bdc6c0
Adaibekov Darkhan <78083548+dak0k@users.noreply.github.com>
2024-09-04 14:53:26 +0500

    feat:homework

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,45 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+const menuItems = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда'];
+
+const paginationNumbers = [1, 2, 3, 4, 5];
+const paginationNumberReverse = paginationNumbers.reverse();
+
+
+function getMenuItem(name: string): string {
+  return name;
+}
+
+
+const aboutCompany = getMenuItem('О компании');
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'mentoring-first-project';
+  title: string = 'mentoring-first-project';
+
+  isShowCatalog: boolean = false;
+  isBigImage: boolean = true;
+  isLowerCase: boolean = true;
+  readonly headerItem1: string = 'Главное';
+  readonly headerItem3: string = 'Каталог';
+
+
+  readonly aboutCompany: string = aboutCompany;
+
+  upperCaseMenuItem: string[] = menuItems;
+  menuItemCase() {
+    this.upperCaseMenuItem = this.upperCaseMenuItem.map(item => 
+      this.isLowerCase ? item.toUpperCase() : item.toLowerCase()
+    );
+    this.isLowerCase = !this.isLowerCase;
+  }
+  newPages: number[] = paginationNumberReverse;
 }
