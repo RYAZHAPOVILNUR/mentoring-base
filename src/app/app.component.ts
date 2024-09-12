dee41cf5b81273cb550990c5bd179399c7446b7e
Elhusseinov <108935010+Elhusseinov@users.noreply.github.com>
2024-09-12 19:30:50 +0500

    Подключил роутинг, создал компоненты, вывел список юзеров в цикле

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,42 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './layout/header.component';
+import { FooterComponent } from "./layout/footer.component";
+
+// let name = 'Elchin';
+
+// if (name==='Elchin') {
+//   console.log('name is Elchin')
+// }
+
+// const func1 = (value: number) => console.log('value is:', value);
+
+// func1(456);
+
+// const blank = (name:string, surname:string) => console.log('Ваше имя:', name, 'Ваша фамилия:', surname)
+// blank('Elchin', 'Husseinov')
+
+// const shopping = (name: string, item1: string, item2: string, item3: string, price: [number, number, number]) => {
+//   return name + ' Пошел в магазин' + ' Он купил ' + item1 + ' Затем купил ' + item2 + ' Еще купил ' + item3 + ' Все это обошлось в:' +  (price[0] + price[1] + price[2])
+// }
+
+// const shoppingResult = shopping('Эльчин', 'Хлеб', 'Айран', 'Борани', [30, 50, 89]);
+
+// console.log(shoppingResult)
+
+// const names = ['Elchin', 'Sabuhi', 'Javanshir', 'Rovsen'];
+
+// names.forEach(
+//   (name) => {
+//     console.log(name)
+//   }
+// )
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'mentoring-first-project';
-}
+export class AppComponent {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,21 @@
+import { Component } from "@angular/core";
+import { NgFor, NgIf } from "@angular/common";
+
+const pages = [1, 2 , 3, 4, 5]
+
+const newPages = pages.reverse()
+
+@Component(
+    {
+        selector: "app-home",
+        templateUrl: "./home.component.html",
+        styleUrl: "home.component.scss",
+        standalone: true,
+        imports: [NgFor, NgIf]
+    }
+)
+export class HomeComponent {
+    isShowImage = true;
+
+    newPages = newPages
+}
