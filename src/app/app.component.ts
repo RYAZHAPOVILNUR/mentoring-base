8f16637e4ebccc77857c8286790f1216ca36b947
Kamila Begimkulova <cuttlefish93@gmail.com>
2024-09-13 02:45:02 +0500

    make 4 tasks from js-loop theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,39 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+const getMenuItemName = (name: string) => name;
+const aboutCompanyName = getMenuItemName('О компании');
+
+const newPages = [5, 4, 3, 2, 1];
+const menuItems = [
+  'Каталог',
+  'Стройматериалы',
+  'Инструменты',
+  'Электрика',
+  'Интерьер и одежда',
+].map((item) => item.toUpperCase());
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+  aboutCompany = aboutCompanyName;
+  isShowPhoto = true;
+  newPages = newPages;
+  isUpperCaseText = true;
+  menuItems = menuItems;
+
+  changeMenuText() {
+    this.menuItems = menuItems.map((item) =>
+      this.isUpperCaseText ? item.toLowerCase() : item.toUpperCase()
+    );
+
+    this.isUpperCaseText = !this.isUpperCaseText;
+  }
 }
