12774fc4f75bb0e1a6952b0ffb5838d3812e3388
Safarov12 <147270022+Safarov12@users.noreply.github.com>
2024-09-13 14:41:35 +0500

    Роутинг, создание компонента, запрос на сервер
    Разделила на отдельные компоненты хидер(новая папка) и мейн боди(оставила в основной папке)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
+<<<<<<< Updated upstream
+=======
+import { NgFor, NgIf} from '@angular/common';
+>>>>>>> Stashed changes
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
+<<<<<<< Updated upstream
   imports: [RouterOutlet],
+=======
+  imports: [RouterOutlet, NgIf,NgFor,],
+>>>>>>> Stashed changes
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
+<<<<<<< Updated upstream
+=======
+
+  readonly  aboutCompany = (item: string) => {
+    return item 
+  }
+  readonly aboutCompanyResult = this.aboutCompany('О компании');
+
+  isShowCatalog = true;
+  isShowBanner = true;
+
+  readonly  newPages = [5,4,3,2,1];
+  readonly pagesNew = this.newPages.map(
+  (item) => {
+    return item;
+  }
+  )
+
+  MenuItems = ['Каталог', ' Стройматериалы',' Инструменты',' Электроника',' Интерьер и одежда']
+
+//  readonly upperCaseMenuItems = this.MenuItems.map (
+//    (item: string) => {
+//      return item.toUpperCase();
+//    }
+//   )
+
+isUpperCase = true;
+
+  changeMenuText () {
+    this.MenuItems = this.MenuItems.map(
+      (item: string) => this.isUpperCase ? item.toLowerCase(): item.toUpperCase()
+      )
+      this.isUpperCase = !this.isUpperCase
+    }
+
+>>>>>>> Stashed changes
   title = 'mentoring-first-project';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { UsersListComponent } from './user-list/user-list.component';
+import { HeaderComponent } from './header/header.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'users', component: UsersListComponent},
+    {path: 'header', component: HeaderComponent},
+];
+
