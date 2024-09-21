72bf27d985f98f5e572ac5a32a22a6e5af0a9ad5
Zhansaule Telisheva <jansaule97@mail.ru>
2024-09-22 00:04:40 +0500

    feat: add typescript interfaces, display data from api on the website, style user list cards + fix basket button style on product cards

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -2,6 +2,36 @@ import { NgFor } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 
+export interface IGeo {
+  lat: string;
+  lng: string;
+}
+
+export interface IAddress {
+  street: string;
+  suite: string;
+  city: string;
+  zipcode: string;
+  geo: IGeo;
+}
+
+export interface ICompany {
+  name: string;
+  catchPhrase: string;
+  bs: string;
+}
+
+export interface IUser {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: IAddress;
+  phone: string;
+  website: string;
+  company: ICompany;
+}
+
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
@@ -12,11 +42,11 @@ import { Component, inject } from '@angular/core';
 export class UsersListComponent {
   readonly apiService = inject(HttpClient);
 
-  users = [];
+  users: IUser[] = [];
 
   constructor() {
     this.apiService
-      .get('https://jsonplaceholder.typicode.com/users')
-      .subscribe((response) => console.log(response));
+      .get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+      .subscribe((response) => (this.users = response));
   }
 }
