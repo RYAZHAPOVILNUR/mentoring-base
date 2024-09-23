d12d252b07fef4cd9381bfe20affbe944a36a727
Zhansaule Telisheva <jansaule97@mail.ru>
2024-09-23 19:39:47 +0500

    feat: add user-card component

---
+++ src/app/users-api.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable, inject } from '@angular/core';
+import { IUser } from './users-list/user.interface';
+
+@Injectable({ providedIn: 'root' })
+export class UsersApiService {
+  readonly apiService = inject(HttpClient);
+
+  getUsers() {
+    return this.apiService.get<IUser[]>(
+      'https://jsonplaceholder.typicode.com/users'
+    );
+  }
+}
--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,23 +1,24 @@
 import { NgFor } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
 import { IUser } from './user.interface';
+import { UsersApiService } from '../users-api.service';
+import { UserCardComponent } from './user-card/user-card.component';
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrl: 'users-list.component.scss',
   standalone: true,
-  imports: [NgFor],
+  imports: [NgFor, UserCardComponent],
 })
 export class UsersListComponent {
-  readonly apiService = inject(HttpClient);
+  readonly usersApiService = inject(UsersApiService);
 
   users: IUser[] = [];
 
   constructor() {
-    this.apiService
-      .get<IUser[]>('https://jsonplaceholder.typicode.com/users')
+    this.usersApiService
+      .getUsers()
       .subscribe((response) => (this.users = response));
   }
 
