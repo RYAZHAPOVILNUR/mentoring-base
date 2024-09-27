d0c92c8f6757cc107412bc56ee6e6ba44f01ce69
Den-is-ON <160344195+Den-is-ON@users.noreply.github.com>
2024-09-27 20:04:06 +0300

    Дз из 6 урока

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,65 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+const switchingPagesInReverse: number[] = [5, 4, 3, 2, 1]
+
+const menuBottomItems = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда']
+
+const upperCaseMenuBottomItems = menuBottomItems.map(
+  (item: string) => {
+    return item.toUpperCase()
+  }
+)
+
+const fun1 = (value: string) => value
+
+const aboutCompany: string = fun1('О компании')
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mentoring-first-project';
+  title: string = 'mentoring-first-project';
+
+  readonly headerItem1 = 'Главная';
+
+  readonly headerItem2 = 'О компании';  
+
+  readonly headerItem3 = 'Каталог';
+
+  readonly header2Item1 = 'Каталог';
+
+  readonly header2Item2 = 'Стройматериалы';
+
+  readonly header2Item3 = 'Инструменты';
+
+  readonly header2Item4 = 'Электрика';
+
+  readonly header2Item5 = 'Интерьер и одежда';
+
+  menuBottomItems: string[] = upperCaseMenuBottomItems
+
+  isShowCatalog: boolean = true;
+
+  isShowImg: boolean = true;
+
+  isUpperCase = true;
+
+  changingLetterCase() {
+    this.menuBottomItems = this.menuBottomItems.map(
+      item => this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
+    )
+    this.isUpperCase = !this.isUpperCase
+  }
+
+  readonly aboutCompany = aboutCompany
+
+  readonly switchingPagesInReverse: number[] = switchingPagesInReverse
+
+  
 }
