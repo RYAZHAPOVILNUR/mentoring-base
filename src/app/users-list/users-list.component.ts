8094ca375be7874707805f162a646ac69d12b19a
Holin1945 <rasel90@inbox.ru>
2024-10-07 00:29:25 +0300

    Доделал ДЗ по роутингу

--- src/app/users-list/users-list.component.ts
+++ src/app/users-list/users-list.component.ts
@@ -1,32 +1,51 @@
-import { NgFor } from "@angular/common";
-import { HttpClient } from "@angular/common/http";
-import { Component, inject } from "@angular/core";
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 
+export interface User {
+  id:             number;
+  name:           string;
+  username:       string;
+  email:          string;
+  address: {
+    street:       string;
+    suite:        string;
+    zipcode:      string;
+    city:         string;
+    geo: {
+      lat:        string;
+      lng:        string;
+    };
+  };
+  phone:          string;
+  website:        string;
+  company: {
+    bs:           string;
+    catchPhrase:  string;
+    name:         string;
+  };
+}
 
 @Component({
   selector: 'app-users-list',
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss',
   standalone: true,
-  imports: [NgFor]
+  imports: [NgFor],
 })
 export class UsersListComponent {
   title = 'mentoring-base';
   readonly apiServise = inject(HttpClient);
-   users: any = [];
+  users: User[] = [];
 
   constructor() {
-    this.apiServise.get('https://jsonplaceholder.typicode.com/users').subscribe(
-      (response: any) => {
+    this.apiServise.get<User[]>('https://jsonplaceholder.typicode.com/users')
+      .subscribe((response: any) => {
         this.users = response;
-        console.log('USERS: ', this.users)
-      }
-    )
+        console.log('USERS: ', this.users);
+      });
   }
   deleteUser(id: number) {
-    this.users = this.users.filter(
-      // @ts-ignore
-      item => item.id !== id
-    )
-  }  
-}  
+    this.users = this.users.filter((item) => item.id !== id);
+  }
+}
