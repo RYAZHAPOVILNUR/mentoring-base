b0fc3f9cb059ab65498206be2fbdf6f6f84f1dea
Anuar <leprickon1990@gmail.com>
2024-10-08 06:28:27 +0600

    domashka-angular-material-08/10/2024

--- src/app/create-user-form/create-user-form.component.ts
+++ src/app/create-user-form/create-user-form.component.ts
@@ -1,11 +1,15 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-create-user-form',
   standalone: true,
-  imports: [ReactiveFormsModule, NgIf, NgFor],
+  imports: [ReactiveFormsModule, NgIf, NgFor, MatButtonModule, MatFormFieldModule, MatInputModule, MatSelectModule],
   templateUrl: './create-user-form.component.html',
   styleUrl: './create-user-form.component.scss'
 })
@@ -24,10 +28,10 @@ export class CreateUserFormComponent {
     // [Validators.email] поле ожидает обязательное заполнение c @
     // [Validators.minLength(5)] поле ожидает минимум 5 символов
     // Validators.pattern("^[a-zA-Zа-яА-я.]*$") ожидает только латиницу и русский алфавит в верхнем и нижнем регистре от A до Z и от А до Я
-    name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern("^[a-zA-Z.]*$")]),
+    name: new FormControl('', [Validators.required, Validators.minLength(2), Validators.pattern("^[a-zA-Zа-яА-Я]*$")]),
     email: new FormControl('', [Validators.required, Validators.email, Validators.minLength(5),]),
     website: new FormControl('', [Validators.required, Validators.minLength(5), Validators.pattern("^[a-zA-Z.]*$")]),
-    companyName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern("^[a-zA-Z.]*$")]),
+    companyName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.pattern("^[a-zA-Zа-яА-Я.]*$")]),
   });
 
   public submitForm(): void {
