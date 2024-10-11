45ecb143545f7c5d81bf32f9736003dc2a7b0bbe
Holin1945 <rasel90@inbox.ru>
2024-10-11 14:26:49 +0300

    Потерялись базовые файлы, восстановил в одной ветке

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,24 @@
 import { NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./header/header.component";
+import { FooterComponent } from "./footer/footer.component";
+
+ 
+const newPages = [5, 4, 3, 2, 1];
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'mentoring-first-project';
+
+  title = 'mentoring-base';
+
+  newPages = newPages;
+
+  isManOnPhoto = true;
 }
