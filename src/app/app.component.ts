8311d4c0529ead48404b0b5449d936f234031c5e
Madiyar Yp <madimadiar96@gmail.com>
2024-10-18 11:15:02 +0500

    Домашка по уроку - УСЛОВНЫЕ ОПЕРАТОРЫ, МАССИВЫ, ФУНКЦИИ, ЦИКЛЫ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,44 @@
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 const menuItems = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда'];
+const upperCaseMenuItems = menuItems.map(item => item.toUpperCase());
 
-const upperCaseMenuItems = menuItems.map(item => item.toUpperCase())
-console.log(upperCaseMenuItems)
+const setMenuName = (name: string) => name;
+const aboutCompany = setMenuName('О компании')
+
+const newPages = [5, 4, 3, 2, 1]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mentoring-first-project';
   isShowCatalog = true;
+  isShowBanner = true;
+  isUpperCase = true;
+  menuItems = menuItems;
+  upperCaseMenuItems = upperCaseMenuItems;
+  newPages = newPages;
 
-  menuItems = upperCaseMenuItems;
+  changeMenuText() {
+    this.menuItems = this.upperCaseMenuItems.map(
+      (item) => {
+        return this.isUpperCase ? item.toUpperCase() : item.toLowerCase()
+      }
+    )
+
+    this.isUpperCase = !this.isUpperCase;
+  }
 
   readonly headerItem1 = 'Главная';
-  readonly headerItem2 = 'О компании';
+  readonly aboutCompany = aboutCompany;
   readonly headerItem3 = 'Каталог';
+
   readonly header2Item1 = 'Каталог';
 }
