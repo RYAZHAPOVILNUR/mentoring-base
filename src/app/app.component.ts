d5fd53549fb9319dd16a7912a670d10de62eeced
Madiyar Yp <madimadiar96@gmail.com>
2024-10-18 10:19:31 +0500

    Сверстал сайт полностью

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+const menuItems = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда'];
+
+const upperCaseMenuItems = menuItems.map(item => item.toUpperCase())
+console.log(upperCaseMenuItems)
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+  isShowCatalog = true;
+
+  menuItems = upperCaseMenuItems;
+
+  readonly headerItem1 = 'Главная';
+  readonly headerItem2 = 'О компании';
+  readonly headerItem3 = 'Каталог';
+  readonly header2Item1 = 'Каталог';
 }
