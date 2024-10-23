ed77f059c55fce775845e6d5e92d1a8f4e3321c0
Zhansaule Telisheva <jansaule97@mail.ru>
2024-10-23 10:36:25 +0500

    feat: add snackbar to create-user component

--- src/app/users-list/create-user/create-user.component.ts
+++ src/app/users-list/create-user/create-user.component.ts
@@ -2,19 +2,28 @@ import { Component, EventEmitter, inject, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { MatDialog } from '@angular/material/dialog';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { CreateUserDialogComponent } from './create-user-dialog/create-user-dialog.component';
 import { IUser } from '../../interfaces/user.interface';
+import { MatSnackBarModule, MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrl: './create-user.component.scss',
-  imports: [MatButtonModule, MatIconModule, MatCardModule],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatDialogModule,
+    MatSnackBarModule,
+  ],
   standalone: true,
 })
 export class CreateUserComponent {
   readonly dialog = inject(MatDialog);
+
+  private _snackBar = inject(MatSnackBar);
 
   @Output()
   createUser = new EventEmitter<IUser>();
@@ -25,6 +34,9 @@ export class CreateUserComponent {
     dialogRef.afterClosed().subscribe((createdResult: IUser) => {
       if (createdResult) {
         this.createUser.emit(createdResult);
+        this._snackBar.open('Пользователь создан!', 'OK', {
+          duration: 3000,
+        });
       }
     });
   }
