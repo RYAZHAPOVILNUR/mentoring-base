cdd1f7a26c8ddf59dccee7be0831546b1f28eb2c
Holin1945 <rasel90@inbox.ru>
2024-10-30 13:13:06 +0300

    Angular Material

--- src/app/create-user-form/create-user-form.ts
+++ src/app/create-user-form/create-user-form.ts
@@ -1,39 +1,58 @@
-import { NgIf } from "@angular/common";
-import { Component, EventEmitter, Output } from "@angular/core";
-import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
+import { NgIf } from '@angular/common';
+import { Component, EventEmitter, Output } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 export function completedValidator(): ValidatorFn {
-    return (control: AbstractControl): ValidationErrors | null => {
-        const value = control.value?.trim().toLowerCase();
-        if (value === 'да' || value ===   'нет') {
-            return null;
-        }
-        return { invalidCompleted: true};
-    };
+  return (control: AbstractControl): ValidationErrors | null => {
+    const value = control.value?.trim().toLowerCase();
+    if (value === 'да' || value === 'нет') {
+      return null;
+    }
+    return { invalidCompleted: true };
+  };
 }
 
 @Component({
-    selector: 'app-create-user-form',
-    templateUrl: './create-user-form.html',
-    styleUrl: './create-user-form.scss',    
-    standalone: true,
-    imports: [ReactiveFormsModule, NgIf],
+  selector: 'app-create-user-form',
+  templateUrl: './create-user-form.html',
+  styleUrl: './create-user-form.scss',
+  standalone: true,
+  imports: [
+    ReactiveFormsModule,
+    NgIf,
+    MatButtonModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatIconModule,
+  ],
 })
 export class CreateUserFormComponent {
-    @Output()
-    createUser = new EventEmitter();
+  @Output()
+  createUser = new EventEmitter();
 
-    public readonly form = new FormGroup({
-        name: new FormControl('', [Validators.required, Validators.minLength(2)]),
-        email: new FormControl('', [Validators.required, Validators.email]),
-        website: new FormControl('', [Validators.required, Validators.minLength(3)]),
-        company: new FormGroup({
-            name: new FormControl('', [Validators.required, Validators.minLength(2)]),  
-    })
-    });
+  public readonly form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    website: new FormControl('', [Validators.required, Validators.minLength(3),]),
+    company: new FormGroup({
+      name: new FormControl('', [Validators.required, Validators.minLength(2)]),
+    }),
+  });
 
-    public submitForm(): void {
-        this.createUser.emit(this.form.value);
-        this.form.reset();
-    }
+  public submitForm(): void {
+    this.createUser.emit(this.form.value);
+    this.form.reset();
+  }
 }
