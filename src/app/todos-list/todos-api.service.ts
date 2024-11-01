952dc2ed929897339c9bbe5e64940bef07051728
Askhat <daniel.han.u88@gmail.com>
2024-11-02 02:17:10 +0600

    Task9

--- src/app/todos-list/todos-api.service.ts
+++ src/app/todos-list/todos-api.service.ts
@@ -1,11 +1,10 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
 
-@Injectable ({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class TodosApiService {
-  
-    readonly apiService = inject(HttpClient);
-    getTodos() {
-        return this.apiService.get('https://jsonplaceholder.typicode.com/todos');
-    }
+  readonly apiService = inject(HttpClient);
+  getTodos() {
+    return this.apiService.get('https://jsonplaceholder.typicode.com/todos');
+  }
 }
--- src/app/todos-list/todos-list.component.ts
+++ src/app/todos-list/todos-list.component.ts
@@ -1,43 +1,37 @@
-import { NgFor } from "@angular/common";
-import { Component, inject } from "@angular/core";
-import { TodoCardComponent } from "./todo-card/todo-card.component";
-import { TodosApiService } from "./todos-api.service";
-
+import { NgFor } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { TodoCardComponent } from './todo-card/todo-card.component';
+import { TodosApiService } from './todos-api.service';
 
 export interface Todo {
-    userId: number,
-    id: number,
-    title: string,
-    comleted: boolean
+  userId: number;
+  id: number;
+  title: string;
+  comleted: boolean;
 }
 @Component({
-    selector: 'app-todos-list',
-    templateUrl: './todos-list.component.html',
-    styleUrl: './todos-list.component.scss',
-    standalone: true,
-    imports: [NgFor, TodoCardComponent]
+  selector: 'app-todos-list',
+  templateUrl: './todos-list.component.html',
+  styleUrl: './todos-list.component.scss',
+  standalone: true,
+  imports: [NgFor, TodoCardComponent],
 })
 export class TodosListComponent {
-    readonly todosApiService = inject(TodosApiService)
-    todos: Todo[] = [];
-    constructor() {
-        this.todosApiService.getTodos().subscribe(
-            (response: any) => {
-                this.todos = response;
-            }
-        )
-    }
+  readonly todosApiService = inject(TodosApiService);
+  todos: Todo[] = [];
+  constructor() {
+    this.todosApiService.getTodos().subscribe((response: any) => {
+      this.todos = response;
+    });
+  }
 
-
-deleteTodo(id: any) {
-    this.todos = this.todos.filter(
-        todo => {
-            if(id===todo.id) {
-                return false
-            } else  {
-                return true;
-            } 
-        }
-    )
-}
+  deleteTodo(id: any) {
+    this.todos = this.todos.filter((todo) => {
+      if (id === todo.id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
 }
