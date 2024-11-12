bcad571dcd9b64bf5da6658cdebd1540944b69c0
sulim3 <sulim2004_18@icloud.com>
2024-11-12 17:25:20 +0100

    add new changes

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,7 +24,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(m
+    expect(compiled.querySelector('h1')?.textContent).toContain
       o, mentoring-first-project');
   });
 });
