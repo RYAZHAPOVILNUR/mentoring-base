a9fa8318603ac420029cdc3932dbf4a48520fbcb
Veronika Soshenko <147270022+Safarov12@users.noreply.github.com>
2024-11-13 21:33:43 +0500

    LocalStorage task
    Сделала задачу с локалстореджем

--- src/app/user-list/user-list.component.ts
+++ src/app/user-list/user-list.component.ts
@@ -1,11 +1,13 @@
-import { ChangeDetectionStrategy, Component, inject, Injectable } from "@angular/core";
-import { AsyncPipe, NgFor} from '@angular/common';
-import { UsersApiService } from "../users-api.service";
-import { UserCardComponent } from "./user-card/user-card.component";
-import { UserService } from "../user.service";
-import { CreateUserFormComponent } from "../create-user-form/create-user-form.component";
-import { CreateUser, User } from "../interfaces/user-interface";
-import { ShadowForUser } from "../directives/user.directive";
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { AsyncPipe, NgFor } from '@angular/common';
+import { UsersApiService } from '../users-api.service';
+import { UserCardComponent } from './user-card/user-card.component';
+import { UserService } from '../user.service';
+import { CreateUserFormComponent } from '../create-user-form/create-user-form.component';
+import { CreateUser, User } from '../interfaces/user-interface';
+import { ShadowForUser } from '../directives/user.directive';
+import { StorageService } from '../local-storage/local-storage.service';
+import { take } from 'rxjs';
 
 @Component({
   selector: 'app-user-list',
@@ -19,26 +21,36 @@ import { ShadowForUser } from "../directives/user.directive";
 export class UsersListComponent {
   readonly usersApiService = inject(UsersApiService);
   readonly usersService = inject(UserService);
+  readonly storageService = inject(StorageService);
+
+  users: User[] = [];
 
   constructor() {
-    this.usersApiService.getUsers().subscribe(
-      (response: any) => {this.usersService.setUsers(response);
-      }
-    )
+    this.users = this.storageService.getUsers();
+
+    if (this.users.length === 0) {
+      this.usersApiService.getUsers().subscribe(
+        (response: User []) => {
+          this.usersService.setUsers(response);
+          this.storageService.saveUserList(response);
+        });
+    } else {
+      this.usersService.setUsers(this.users);
+    }
   }
 
   deleteUser (id: number) {
     this.usersService.deleteUser(id);
+    this.updateLocalStorage();
   }
 
   editUser (user: User) {
-    this.usersService.editUser(
-      user
-    );
+    this.usersService.editUser(user);
+    this.updateLocalStorage();
   }
 
-  public createUser (formData: CreateUser) {
-    this.usersService.createUser({
+  createUser (formData: CreateUser) {
+    const newUser: User = {
       id: new Date().getTime(),
       name: formData.name,
       email: formData.email,
@@ -46,6 +58,14 @@ export class UsersListComponent {
       company: {
         name: formData.company.name,
       },
+    };
+    this.usersService.createUser(newUser);
+    this.updateLocalStorage();
+  }
+
+  private updateLocalStorage (): void {
+    this.usersService.getUsers().pipe(take(1)).subscribe((users: User[]) => {
+      this.storageService.saveUserList(users);
     });
   }
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from "@angular/core";
-import { BehaviorSubject } from "rxjs";
-import { User } from "./interfaces/user-interface";
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { User } from './interfaces/user-interface';
 
 @Injectable ({providedIn: 'root'})
 
@@ -8,31 +8,26 @@ export class UserService {
   private userSubject$ = new BehaviorSubject<User[]>([]);
   users$ = this.userSubject$.asObservable();
 
+  getUsers (): Observable<User[]> {
+    return this.userSubject$.asObservable();
+  }
+
   setUsers (users: User[]) {
     this.userSubject$.next (users);
   }
 
   editUser (editedUser: User) {
     this.userSubject$.next(
-      this.userSubject$.value.map(
-        user => {
-          if (user.id === editedUser.id) {
-            return editedUser
-            } else {
-            return user
-            }
-          }  
-      )
-    )
+      this.userSubject$.value.map(user => user.id === editedUser.id ? editedUser : user)
+    );
   }
-
 
   createUser (user: User) {
     const existingUser = this.userSubject$.value.find(
       (currentElement) => currentElement.email === user.email
     );
 
-    if (existingUser !== undefined) {
+    if (existingUser) {
       alert ('Такой пользователь уже зарегистрирован'); 
     } else {
       this.userSubject$.next([...this.userSubject$.value, user]);
@@ -42,14 +37,7 @@ export class UserService {
 
   deleteUser (id: number) {
     this.userSubject$.next(
-      this.userSubject$.value.filter(
-        item => {
-          if (id === item.id) {
-            return false 
-          } else {
-            return true;}
-        }  
-      )
-    )
+      this.userSubject$.value.filter(item => item.id !== id)
+    );
   }
 }
