ffb767e6ad7e6b31aaa84795a6be5a6bb924eee7
Elnur <136242453+Elnurio95@users.noreply.github.com>
2024-11-19 19:31:11 +0300

    home tasks (except task*

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+
+function getMenuName(menuName: string) {
+  return menuName; 
+}
+
+const newName = getMenuName("О компании"); 
+
+const newPages = [1, 2, 3, 4, 5]; 
+
+const newArray = newPages.reverse(); 
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgIf, NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
+
+
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  readonly aboutCompany = newName; 
+  readonly newPages = newPages;
+
+  isShowImg = false; 
 }
+
+
+
