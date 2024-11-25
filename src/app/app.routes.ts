37a474b08a2e206b0efea5dfa09095d67e680030
Radmil Khasanov <aminasabirova@macbookair.zte.com.cn>
2024-11-25 21:13:27 +0400

    homework-routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { UsersComponent } from './components/users/users.component';
+import { HomepageComponent } from './components/homepage/homepage.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'users',
+  component: UsersComponent,
+  },
+  {
+    path: '',
+    component: HomepageComponent,
+  }
+];
