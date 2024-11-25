ab36ea91836d6c71ad858bce55f2b34f2a50c3cd
Radmil Khasanov <aminasabirova@macbookair.zte.com.cn>
2024-11-25 22:01:21 +0400

    add components

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,62 @@
+import { NgFor, NgIf } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+const menuItems: string[] = [
+  'Каталог',
+  'Стройматериалы',
+  'Инструменты',
+  'Электрика',
+  'Интерьер и одежда',
+];
+
+const upperCaseMenuItems: string[] = menuItems.map((item: string): string => {
+  return item.toUpperCase();
+});
+
+const newPages: number[] = [5, 4, 3, 2, 1];
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [ NgFor, NgIf, RouterLink],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.scss'
+})
+export class HeaderComponent {
+  isShowCatalog = false;
+
+  isShowImg: boolean = true;
+
+  readonly headerItem1 = 'Главная';
+
+  readonly headerItem3 = 'Каталог';
+
+  readonly header2Item1 = upperCaseMenuItems[0];
+
+  readonly header2Item2 = 'Cтройматериалы';
+
+  readonly header2Item3 = 'Инструменты';
+
+  readonly header2Item4 = 'Электрика';
+
+  readonly header2Item5 = 'Интерьер и одежда';
+
+  readonly aboutCompany = 'О компании';
+
+  readonly newtab = 'Пользователи';
+
+  readonly newPages: number[] = newPages;
+
+  menuItems: string[] = upperCaseMenuItems;
+
+  isUpperCase = true;
+
+  changeMenuText() {
+    this.menuItems = upperCaseMenuItems.map((item) =>
+      this.isUpperCase ? item.toLowerCase() : item.toUpperCase()
+    );
+
+    this.isUpperCase = !this.isUpperCase;
+  }
+}
---
+++ src/app/components/users/users.component.ts
@@ -0,0 +1,63 @@
+import { NgFor } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+
+const apiservise = '';
+
+export interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  adress: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+phone: string;
+website: string;
+company: {
+  name: string;
+  catchphrase: string;
+  bs: string;
+};
+}
+
+@Component({
+  selector: 'app-users',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './users.component.html',
+  styleUrl: './users.component.scss',
+})
+export class UsersComponent {
+  readonly apiservise = inject(HttpClient);
+
+  users: User [] = [];
+
+  constructor() {
+    this.apiservise
+      .get('https://jsonplaceholder.typicode.com/users')
+      .subscribe((response: any) => {
+        this.users = response;
+        console.log('USERS:', this.users);
+      });
+  }
+
+  deleteUser(id: number) {
+    this.users = this.users.filter(
+      item => {
+        if (id === item.id) {
+          return false
+        } else {
+          return true;
+        }
+      }
+    );
+  }
+}
