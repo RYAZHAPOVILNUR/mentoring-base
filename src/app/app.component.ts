42af7c4f3df7c382eacb3b5dc12730a652e22c7a
Elnur <136242453+Elnurio95@users.noreply.github.com>
2024-12-05 12:52:52 +0300

    component tasks

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { headerComponent } from "./header/header.component";
 
 function getMenuName(menuName: string) {
   return menuName; 
@@ -13,21 +13,14 @@ const newPages = [1, 2, 3, 4, 5];
 
 const newArray = newPages.reverse(); 
 
-//1. Каждое слово в массиве сделать маленьким шрифтом(или большим) 
-//2. Потом сделать сравнение: если в массиве слова все с маленькой буквой, при клике сделать всё большими буквами, или же наоборот 
 
 let menuItems = ['Каталог','Стройматериалы','Инструменты','Электрика','Интерьер и одежда']; 
-
-// function changeMenuItems() {
-//   return menuItems.map((item:string) => item.toUpperCase()); 
-// }
-
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NgIf, NgFor],
+  imports: [RouterOutlet, NgIf, NgFor, headerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/users-list/users-list.component.ts
@@ -0,0 +1,28 @@
+import { NgFor } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, inject, Injectable } from "@angular/core";
+
+
+@Component ({
+    selector: 'app-users-list', 
+    templateUrl: './users-list.component.html', 
+    styleUrl: './users-list.component.scss',
+    standalone: true,  
+    imports: [NgFor]
+})
+
+
+
+export class UsersListComponent {
+    readonly apiService = inject(HttpClient); 
+    users = []; 
+
+    constructor() {
+        this.apiService.get('https://jsonplaceholder.typicode.com/users').subscribe(
+            (response:any) => {
+                this.users = response;
+                console.log('USERS: ', this.users) 
+            }
+        )
+    }
+}
