57dfc53dfa34ef7f76393fcca68a385032f5f03d
Khusein <Khusein@gmail.com>
2024-12-27 19:03:04 +0300

    refactor: deleted header.component, deleted inline styles

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { UsersListComponent } from './users-list/users-list.component';
-import { HeaderComponent } from './header/header.component';
 import { ContentComponent } from './content/content.component';
 import { TodoListComponent } from './todo-list/todo-list.component';
 
