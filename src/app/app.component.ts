acc7ef9c47dda5a26683491fa037f57d8f99a810
Khusein <Khusein@gmail.com>
2024-12-27 18:54:00 +0300

    refactor: deleted header.component, deleted inline styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
 import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './header/header.component';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+const menuNameItem = (menuName: string) => {
+  return menuName;
+};
+
+const menuItems = [
+  'Каталог',
+  'Стройматериалы',
+  'Инструменты',
+  'Электрика',
+  'Интерьер и одежда',
+];
+
+const upperCaseItems = menuItems.map((i) => i.toUpperCase());
+const lowerCaseItems = menuItems.map((i) => i.toLowerCase());
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, NgIf, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
@@ -14,4 +28,13 @@ export class AppComponent {
   title = 'mentoring-first-project';
 
   isShowMainPicture = true;
+  isShowMain = true;
+  isUpperCase = false;
+
+  upperCaseItems = upperCaseItems;
+  lowerCaseItems = lowerCaseItems;
+
+  readonly header_item_1 = 'Главная';
+  readonly aboutCompany = menuNameItem('О компании');
+  readonly header_item_3 = 'Каталог';
 }
--- src/app/todo-list/todo-card/todo-card.component.ts
+++ src/app/todo-list/todo-card/todo-card.component.ts
@@ -1,26 +1,22 @@
-import { Component, EventEmitter, Input, Output } from "@angular/core";
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Todo } from '../todo-list.component';
 
 
 @Component({
-    selector: 'todo-card-root',
-    templateUrl: './todo-card.component.html',
-    styleUrl: './todo-card.component.scss',
-    standalone: true,
-    imports: []
+  selector: 'todo-card-root',
+  templateUrl: './todo-card.component.html',
+  styleUrl: './todo-card.component.scss',
+  standalone: true,
+  imports: [],
 })
-
 export class TodoCardComponent {
-    @Input()
+  @Input()
+  todoCard: any;
 
-    todoCard: any
+  @Output()
+  deleteTodoCard = new EventEmitter();
 
-    @Output()
-
-    deleteTodoCard = new EventEmitter()
-
-    onDeleteTodo(todoCardId: number) {
-        this.deleteTodoCard.emit(todoCardId)
-    }
-
+  onDeleteTodo(todoCardId: number) {
+    this.deleteTodoCard.emit(todoCardId);
+  }
 }
