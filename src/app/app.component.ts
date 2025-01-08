b69a2f7dd71a63eedc454637cbe2ba5ba38ec621
NolOdin <mr.ciborg@outlook.com>
2025-01-08 16:37:51 +0300

    5 task done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,8 +6,20 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  headerItem1 = 'Главная';
+  headerItem2 = 'О компании';
+  headerItem3 = 'Каталог';
+
+  secondHeaderItem1 = 'Стройматериалы';
+  secondHeaderItem2 = 'Инструменты';
+  secondHeaderItem3 = 'Электрика';
+  secondHeaderItem4 = 'Интерьер и одежда';
+
+
+  
 }
