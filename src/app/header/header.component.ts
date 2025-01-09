7b921f46c365fb4f4bc5b21b171d49d7026fc286
ибрахим <ibraximaliev45@mail.ru>
2025-01-09 20:21:59 +0300

    feat: Реализован код базовых конструкций на TypeScript

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,13 +1,40 @@
+import { NgFor, NgIf } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  headerItem1 = 'Главная';
+  headerItem3 = 'Каталог';
+  menuItems = ['Каталог', 'Стройматериалы', 'Инструменты', 'Электрика', 'Интерьер и одежда']
+  isUpperCaseState: boolean = false;
+  showCatolog: boolean = true;
+  aboutCompany: string = '';
+
+  constructor() {
+    this.aboutCompany = this.getMenuItem('О компании');
+  }
+
+  isShowCatalog() {
+    this.showCatolog = !this.showCatolog
+  }
+  
+  getMenuItem(menuItem: string): string {
+    return menuItem;
+  }
+
+  isUpperCase(){
+    this.isUpperCaseState = !this.isUpperCaseState
+
+    this.menuItems = this.menuItems.map(value =>
+      this.isUpperCaseState ? value.toLocaleUpperCase() : value.toLocaleLowerCase()
+    );
+  }
 }
