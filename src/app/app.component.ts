34fef505dd843eb4de5cbf7d8e95e68ae750c06c
AbuAsiay <AbuAsiay@gmail.com>
2025-01-17 08:34:42 +0200

    Создал переменные. урок5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,15 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'mentoring-first-project';
+
+  readonly menunavitem1 = 'Каталог';
+
+  readonly menunavitem2 = 'Стройматериалы';
+
+  readonly menunavitem3 = 'Инструменты';
+
+  readonly menunavitem4 = 'Электрика';
+
+  readonly menunavitem5 = 'Интерьер и одежда';
 }
+
