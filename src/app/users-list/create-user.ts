ba1f348373e6a2d7b1321d8ad07a54c8fafda2f6
Hadjimurad <hadjimurad997@mail.ru>
2025-01-30 01:00:23 +0400

    Refactor: удалил ненужные поля createUser

--- src/app/users-list/create-user.ts
+++ src/app/users-list/create-user.ts
@@ -1,23 +1,9 @@
 export interface createUser {
   id: number,
   name: string,
-  username: string,
-  email: string,
-  addres: {
-    street: string,
-    suite: string,
-    city: string,
-    zipcode: string,
-    geo: {
-      lat: number,
-      lng: number
-    }
-  },
-  phone: number,
+  email: string
   website: string,
-    company: {
-      name: string,
-      catchPhrase: string,
-      bs: string
+  company: {
+    name: string,
   }
 }
