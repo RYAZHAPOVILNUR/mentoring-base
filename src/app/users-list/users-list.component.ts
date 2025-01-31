517c085ff7ad638ad7ae3114046c061d6a55634b
MusaSagov <sagov82@mail.ru>
2025-01-31 23:42:51 +0300

    feat lesson 7 task 1-5

---
+++ src/app/users-list/users-list.component.ts
@@ -0,0 +1,35 @@
+import { NgFor } from "@angular/common";
+import { HttpClient } from "@angular/common/http";
+import { Component, inject } from "@angular/core";
+import { User } from "./user.interface";
+
+@Component({
+  selector: "app-users-list",
+  templateUrl: "./users-list.component.html",
+  styleUrl: "./users-list.component.scss",
+  standalone: true,
+  imports: [NgFor],
+})
+export class UsersListComponent {
+  readonly apiService = inject(HttpClient);
+  users: User[] = [];
+
+  constructor() {
+    this.apiService
+      .get("http://jsonplaceholder.typicode.com/users")
+      .subscribe((response: any) => {
+        this.users = response;
+        console.log("USERS: ", this.users);
+      });
+  }
+
+  deleteUser(id: number) {
+    this.users = this.users.filter((item) => {
+      if (id === item.id) {
+        return false;
+      } else {
+        return true;
+      }
+    });
+  }
+}
